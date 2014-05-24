f77b2dd44682935bb98d3f97f84a29885e941ae8
G580 mgoyal <munishapc@gmail.com>
2014-05-24 08:11:22 +0530

    user_id of word can now be nil, and removed user_id field from words form

--- app/models/word.rb
+++ app/models/word.rb
@@ -8,7 +8,6 @@ class Word < ActiveRecord::Base
   # validations
   validates :word, presence: true, uniqueness: true, length: {maximum: 25}
   validates :trick, length: {maximum: 100}
-  validates :user_id, presence: true
   validates :additional_info, length: {maximum: 2048}
 
   # callbacks

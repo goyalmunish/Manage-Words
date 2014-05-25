083c45c1038b08c96478961de135a143be28042c
G580 mgoyal <munishapc@gmail.com>
2014-05-25 09:04:20 +0530

    added user seed data for production

--- db/seeds/02_production.rb
+++ db/seeds/02_production.rb
@@ -6,5 +6,8 @@ if ENV['RAILS_ENV']=='production'
   sleep(5.seconds.to_i)
 
   # SEEDING
+  if User.count == 0
+    User.create!(:first_name => 'Munish', :last_name => 'Goyal', :type => 'Admin', :email => 'munishapc@gmail.com', :password => 'munishgoyal', :password_confirmation => 'munishgoyal')
+  end
   
 end

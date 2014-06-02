94bfc1efd5efb6fa21ff6277c6fd18ba6eddfe85
Munish Goyal Nagarro Desktop <munish.goyal@nagarro.com>
2014-06-02 13:08:01 +0530

    added caching to flags#index

--- app/helpers/flags_helper.rb
+++ app/helpers/flags_helper.rb
@@ -6,4 +6,11 @@ module FlagsHelper
   def flag_name_value_and_desc(flag)
     "#{flag.name}-#{flag.value} | #{flag.desc}"
   end
+
+  def flags_index_fragment_name
+    name = Array.new
+    name << "COUNT-#{Flag.count}"
+    name << "MAX-UPDATED-AT-#{Flag.maximum(:updated_at).getutc}"
+    return name.join('__')
+  end
 end

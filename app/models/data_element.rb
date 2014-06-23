d379b3acdb9e14734b271d109663b1fd05ad69ef
Munish Goyal Nagarro Desktop <munish.goyal@nagarro.com>
2014-06-23 14:51:52 +0530

    refactoring data_element.rb and word_data_element.rb - Level 1

--- app/models/data_element.rb
+++ app/models/data_element.rb
@@ -1,39 +1,59 @@
 class DataElement
-  attr_reader :email
-  attr_accessor :words
+  attr_reader :email, :words
 
-  def initialize(email, words = Array.new())
-    @email = email
-    @words = words
+  def initialize(args)
+    args = defaults.merge(args)
+    @email = args[:email]
+    @words = args[:words]
   end
 
-  def append_to_words(word_data_element)
-    self.words << word_data_element
+  def defaults
+    {:words => Array.new}
   end
 
   def to_h
     {:email => email, :words => words}
   end
 
+  def append_to_words(word_data_element)
+    self.words << word_data_element
+  end
+
+  # has external dependencies
+  def self.all_users_with_eager_loaded_words_and_flags
+    User.includes(:words => :flags).all
+  end
+
   # for backup
-  def self.data_backup(users = User.includes(:words => :flags).all)
+  # expects passed users argument to respond to words
+  # expects each word to respond to flags
+  # uses WordDataElement class directly
+  def self.data_backup(users = self.all_users_with_eager_loaded_words_and_flags)
     data_elements = Array.new
     users.each do |user|
-      data_elements << DataElement.new(user.email, WordDataElement.word_data_backup(user.words.includes(:flags)))
+      data_elements << DataElement.new(
+          :email => user.email,
+          :words => WordDataElement.word_data_backup(user.words.includes(:flags)))
     end
     return data_elements
   end
 
   # for backup restore
+  # uses User class directly
+  # uses WordDataElement class directly
   def self.restore_data_backup(array_data)
     word_count = 0
     array_data.each do |hash_data|
       user = User.where(:email => hash_data['email']).first
       if user
-        temp_count = WordDataElement.restore_word_data_backup(user.id, hash_data['words'])
+        temp_count = WordDataElement.restore_word_data_backup(user, hash_data['words'])
         word_count += temp_count
       end
     end
     return word_count
   end
+
+  protected
+
+  attr_writer :words
 end
--- app/models/word_data_element.rb
+++ app/models/word_data_element.rb
@@ -1,29 +1,43 @@
 class WordDataElement
-  attr_reader :word, :trick, :additional_info
-  attr_accessor :flags_attributes
+  attr_reader :word, :trick, :additional_info, :flags_attributes
 
-  def initialize(word, trick, additional_info, flags_attributes = Array.new)
-    @word = word
-    @trick = trick
-    @additional_info = additional_info
-    @flags_attributes = flags_attributes
+  def initialize(args)
+    args = defaults.merge(args)
+    @word = args[:word]
+    @trick = args[:trick]
+    @additional_info = args[:additional_info]
+    @flags_attributes = args[:flags_attributes]
   end
 
-  def append_to_flags_attributes(name, value)
-    self.flags_attributes << {:name => name, :value => value}
+  def defaults
+    {:flags_attributes => Array.new}
   end
 
   def to_h
     {:word => word, :trick => trick, :additional_info => additional_info, :flags_attributes => flags_attributes}
   end
 
+  def append_to_flags_attributes(args)
+    self.flags_attributes << args.select{ |key, value| [:name, :value].include? key}
+  end
+
+  # has external dependencies
+  def self.all_words_with_eager_loaded_flags
+    Word.includes(:flags).all
+  end
+
   # for backup
-  def self.word_data_backup(words = Word.includes(:flags).all)
+  # expects passed words argument to responds to flags
+  # expects each flag to respond to name and value
+  def self.word_data_backup(words = self.all_words_with_eager_loaded_flags)
     word_data_elements = Array.new
     words.each do |word|
-      temp_word_data_element = WordDataElement.new(word.word, word.trick, word.additional_info)
+      temp_word_data_element = WordDataElement.new(
+          :word => word.word,
+          :trick => word.trick,
+          :additional_info => word.additional_info)
       word.flags.each do |flag|
-        temp_word_data_element.append_to_flags_attributes(flag.name, flag.value)
+        temp_word_data_element.append_to_flags_attributes(:name => flag.name, :value => flag.value)
       end
       word_data_elements << temp_word_data_element.to_h
     end
@@ -32,8 +46,9 @@ class WordDataElement
   end
 
   # for backup restore
-  def self.restore_word_data_backup(user_id, array_data)
-    user = User.find(user_id)
+  # expects passed user argument to respond to words and email
+  # uses Flag class directly
+  def self.restore_word_data_backup(user, array_data)
     word_count = 0
     array_data.each do |hash_data|
       # extracting flag_attributes
@@ -56,7 +71,11 @@ class WordDataElement
         end
       end
     end
-    Rails.logger.info "Restored #{word_count} words for user_id #{user_id}\n"
+    Rails.logger.info "Restored #{word_count} words for email #{user.email}\n"
     return word_count
   end
+
+  protected
+
+  attr_writer :flags_attributes
 end

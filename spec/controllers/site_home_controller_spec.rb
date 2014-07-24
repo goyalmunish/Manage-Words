bdf2a3a183e4fc8a1a8c1ba6cfa3b748d789f05c
Munish Goyal Nagarro Desktop <munish.goyal@nagarro.com>
2014-07-24 11:07:07 +0530

    other controller specs are made blank

--- spec/controllers/site_home_controller_spec.rb
+++ spec/controllers/site_home_controller_spec.rb
@@ -3,52 +3,24 @@ require 'spec_helper'
 RSpec.describe SiteHomeController, :type => :controller do
 
   describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
   end
 
   describe "GET 'sign_up'" do
-    it "returns http success" do
-      get 'sign_up'
-      response.should be_success
-    end
   end
 
   describe "GET 'sign_in'" do
-    it "returns http success" do
-      get 'sign_in'
-      response.should be_success
-    end
   end
 
   describe "GET 'contact_us'" do
-    it "returns http success" do
-      get 'contact_us'
-      response.should be_success
-    end
   end
 
   describe "GET 'about_us'" do
-    it "returns http success" do
-      get 'about_us'
-      response.should be_success
-    end
   end
 
   describe "GET 'faqs'" do
-    it "returns http success" do
-      get 'faqs'
-      response.should be_success
-    end
   end
 
   describe "GET 'help'" do
-    it "returns http success" do
-      get 'help'
-      response.should be_success
-    end
   end
 
 end

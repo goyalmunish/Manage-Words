c2c50f94dc948ed234fc85d88497bde58f5cd54b
Munish Goyal Nagarro Desktop <munish.goyal@nagarro.com>
2014-08-21 14:45:41 +0530

    writing acceptance test for signin

---
+++ spec/features/sign_in_process_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "the signin process",:type => :feature do
+  before(:each) do
+    @user = create(:user, :password => 'password', :password_confirmation => 'password')
+  end
+  context "with correct credentials" do
+    it "signs me in" do
+      visit '/users/sign_in'
+      within('#new_user') do
+        fill_in 'user_email', :with => @user.email
+        fill_in 'user_password', :with => 'password'
+      end
+      click_on 'Sign in'
+      expect(page).to have_content(@user.email)
+    end
+  end
+  context "with incorrect credentials" do
+    it "displays 'Invalid email or password' error" do
+      visit '/users/sign_in'
+      within('#new_user') do
+        fill_in 'user_email', :with => @user.email
+        fill_in 'user_password', :with => 'wrong_password'
+      end
+      click_on 'Sign in'
+      expect(page).to have_content('Invalid email or password')
+    end
+  end
+end

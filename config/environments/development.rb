2cc3b01497d8429e1e2065fd455a5ce2cd27a763
Munish G580 <munishapc@gmail.com>
2014-11-02 01:58:07 +0530

    enabled dalli caching in production, disabled caching in development, and enabled per word fragment caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,11 +11,11 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local = true
-  # config.action_controller.perform_caching = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
+  # config.action_controller.perform_caching = true
 
   # Configuring cache store
-  config.cache_store = :dalli_store, {pool_size: 12}
+  # config.cache_store = :dalli_store, {pool_size: 12}
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :dalli_store, {namespace: 'word-list', compress: true, pool_size: 12}
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"

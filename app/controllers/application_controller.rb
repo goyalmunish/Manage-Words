d300bf9364e128f371f24cc83e1abea2086cdb4f
Munish G580 <munishapc@gmail.com>
2015-06-01 07:02:02 +0530

    ApplicationController: Refactored application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  # handling any left unhandled exception
-
   # I want to run below code only in case of production
-  if Rails.env == 'production'
+  if %w(production).include? Rails.env
     rescue_from Exception do |ex|
       # displaying error
       flash[:alert] = 'Error: ' + ex.message
@@ -12,7 +8,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  # handling any left unhandled exception
   protect_from_forgery with: :exception
+  # protect_from_forgery with: :null_session
 
   acts_as_token_authentication_handler_for User
   before_filter :authenticate_user!

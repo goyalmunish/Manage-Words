58e8035cf44ef35329b98116b21aa90bf30d481b
Munish Goyal <munish.goyal@universumglobal.com>
2016-04-09 16:19:20 +0530

    Implement word_search box and full_search box using component and without reloading model

--- frontend/app/components/word-collection.js
+++ frontend/app/components/word-collection.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  filteredWords: Ember.computed('word_search', 'full_search', function() {
+    var filtered_words = this.get('words');
+    var word_search = this.get('word_search');
+    var full_search = this.get('full_search');
+
+    if(word_search) {
+      console.log("Word search for: " + word_search);
+      var re_word_search = new RegExp(word_search);
+
+      // filtered_words = words.filterBy('word', word_search);
+      filtered_words = filtered_words.filter(function(item, index, self) {
+        var result = re_word_search.test(item.get('word'));  // Note: Here we used item.get('word') and not item.word. The later one would work only in templates.
+        return result;
+      });
+    }
+
+    if(full_search) {
+      console.log("Full search for: " + full_search);
+      var re_full_search = new RegExp(full_search);
+
+      filtered_words = filtered_words.filter(function(item, index, self) {
+        var result = re_full_search.test(item.get('word')) || re_full_search.test(item.get('trick')) || re_full_search.test(item.get('additionalInfo'));
+        return result;
+      });
+    }
+
+    return filtered_words;
+  })
 });
--- frontend/app/routes/words.js
+++ frontend/app/routes/words.js
@@ -1,14 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  queryParams: {
-    word_search: {
-      refreshModel: true
-    },
-    full_search: {
-      refreshModel: true
-    }
-  },
   model() {
     return this.store.findAll('word');
   }

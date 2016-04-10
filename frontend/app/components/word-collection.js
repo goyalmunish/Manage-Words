0d566138d174dd1a3601d03364c7a5082400990b
Munish Goyal <munish.goyal@universumglobal.com>
2016-04-10 23:42:08 +0530

    Implement top 10 suggestions using component actions and key-up event
    Remove un-necessary 'index' and 'self' argument from filter method
    call

--- frontend/app/components/word-collection.js
+++ frontend/app/components/word-collection.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   // Properties used as arguments: words, word_search, full_search
+
+  // Other properties
+  suggestions: null,
+
+  // Computed propreties
   filteredWords: Ember.computed('word_search', 'full_search', function() {
     var filtered_words = this.get('words');
     var word_search = this.get('word_search');
@@ -12,7 +17,7 @@ export default Ember.Component.extend({
       var re_word_search = new RegExp(word_search);
 
       // filtered_words = words.filterBy('word', word_search);
-      filtered_words = filtered_words.filter(function(item, index, self) {
+      filtered_words = filtered_words.filter(function(item) {
         var result = re_word_search.test(item.get('word'));  // Note: Here we used item.get('word') and not item.word. The later one would work only in templates.
         return result;
       });
@@ -24,7 +29,7 @@ export default Ember.Component.extend({
       console.log("Full search for: " + full_search);
       var re_full_search = new RegExp(full_search);
 
-      filtered_words = filtered_words.filter(function(item, index, self) {
+      filtered_words = filtered_words.filter(function(item) {
         var result = re_full_search.test(item.get('word')) || re_full_search.test(item.get('trick')) || re_full_search.test(item.get('additionalInfo'));
         return result;
       });
@@ -33,5 +38,26 @@ export default Ember.Component.extend({
     }
 
     return filtered_words;
-  })
+  }),
+
+  numberOfFilteredWords: Ember.computed('filteredWords', function() {
+    // Note that calling just `length` works in view but not here
+    return this.get('filteredWords').get('length');
+  }),
+
+  // Actions
+  actions: {
+    // this functionality is implemented using actions for learning purpose, though
+    // it could be easily done using computed property based on filteredWords
+    calculateSuggestions() {
+      if (this.get('word_search') || this.get('full_search')) {
+        var suggestions = this.get('filteredWords').map(function(item) {
+          return item.get('word');
+        });
+        this.set('suggestions', suggestions.slice(0,9));
+      } else {
+        this.set('suggestions').clear();
+      }
+    }
+  }
 });

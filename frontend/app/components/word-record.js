4297ae284496f87b80660c34d6f0b4d43a40cbcd
Munish Goyal <munish.goyal@universumglobal.com>
2016-04-15 08:58:35 +0530

    Add functionality of toggling word fields Editable/Readonly

--- frontend/app/components/word-record.js
+++ frontend/app/components/word-record.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   // Properties used as arguments: word
+
+  // Other properties
   isCreatedUpdatedShowing: false,
+  readonly: true,
+
+  // Actions
   actions: {
     showCreatedUpdated() {
       // Note that the action doesn't return anything
@@ -11,6 +16,20 @@ export default Ember.Component.extend({
     hideCreatedUpdated() {
       // Note that the action doesn't return anything
       this.set('isCreatedUpdatedShowing', false);
+    },
+    toggleReadonly() {
+      var readonly = this.get('readonly');
+      if(readonly) {
+        readonly = false;
+      }
+      else {
+        readonly = true;
+      }
+      this.set('readonly', readonly);
+    },
+    save() {
+      console.log("Saving to server...");
+      alert("Yet to be implemented");
     }
   }
 });

0bf72b14ec1fdb46111fee1b41f253795c6ed9f0
Munish Goyal <munish.goyal@universumglobal.com>
2016-05-04 07:57:11 +0530

    Notify user to load 'users' first
    In order to display custom dictionary links for a particular user and
    work, the user is suppose to load 'users' fist. TODO: To fix it later.

--- frontend/app/components/user-word-dictionary-links.js
+++ frontend/app/components/user-word-dictionary-links.js
@@ -11,25 +11,31 @@ export default Ember.Component.extend({
     let word = this.get('word');
     let dictionaries = user.get('dictionaries');
     let links = [];
-    dictionaries.forEach(function(dict) {
-      // generating the link url
-      let dict_name = dict.get('name');
-      let dict_url = dict.get('url');
-      let dict_separator = dict.get('separator');
-      let dict_suffix = dict.get('suffix');
-      let joined_words = word.split(" ").join(dict_separator);
-      let link_url = '';
-      link_url += dict_url;
-      link_url += joined_words;
-      if(dict_suffix) { link_url += dict_suffix; }
-      // generating the link
-      let a_link = `<div class="dictionary_link"><a href=${link_url} target ='_blank'>${dict_name}</a></div>`;
-      let a_link_html_safe = Ember.String.htmlSafe(a_link);
-      // adding to links array
-      links.addObject(a_link_html_safe);
-    });
+    if(dictionaries === undefined) {
+      // alert("Would you mind first loading the Users!");
+      console.log("Warning: In order to load the dictionary links, please load users first!");
+    }
+    else {
+      dictionaries.forEach(function(dict) {
+        // generating the link url
+        let dict_name = dict.get('name');
+        let dict_url = dict.get('url');
+        let dict_separator = dict.get('separator');
+        let dict_suffix = dict.get('suffix');
+        let joined_words = word.split(" ").join(dict_separator);
+        let link_url = '';
+        link_url += dict_url;
+        link_url += joined_words;
+        if(dict_suffix) { link_url += dict_suffix; }
+        // generating the link
+        let a_link = `<div class="dictionary_link"><a href=${link_url} target ='_blank'>${dict_name}</a></div>`;
+        let a_link_html_safe = Ember.String.htmlSafe(a_link);
+        // adding to links array
+        links.addObject(a_link_html_safe);
+      });
+    }
 
-    // didn't have to put above block in then() block
+    // Note: didn't have to put above block in then() block
     // dictionaries.then(function(dictionaries) {
     // });
 

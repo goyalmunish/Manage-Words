44087588b8b4f828836c36d2b573e4e00eae37ff
Munish Goyal <munish.goyal@universumglobal.com>
2016-05-11 20:29:37 +0530

    Conditionally skip constants setup from an initializer file
    Skip constants setup (as defined in 01_settings_based_contants.rb) when
    AppSetting model is not loaded. For example, this happens in case of
    `rake db:migrate` command and `rspec spec/models`.
    Note: This fixes issue as described in following commit:
    Commit Number: d6e3df73fa4e9b0018a2b8d04f2b786e3eb0b45b
    
    Current Status of Tests: -----------------------------------------------
    $ rspec spec/models
    Finished in 2.91 seconds (files took 3.58 seconds to load)
    138 examples, 0 failures
    
    $ rspec  spec/controllers/app_settings_controller_spec.rb
    Finished in 0.12351 seconds (files took 2.4 seconds to load)
    18 examples, 0 failures
    
    Words & Flags Status: --------------------------------------------------
    No. of words: 2326
    No. of associated flags: 1450

--- config/initializers/01_custom_constants/01_settings_based_constants.rb
+++ config/initializers/01_custom_constants/01_settings_based_constants.rb
@@ -3,65 +3,72 @@
 # Note: the loading of '01_custom_constants' at the earliest is required (except before 00_active_admin.rb), as these values are used in other initializer files
 
 
-#### WORKAROUND ###################################
-# note that as we are referencing AppSetting class, it has to exist before running this
+# Note that as we are referencing AppSetting class, it has to exist before running this script,
 # otherwise it will create problem if you try to run "rake db:migrate" on newly created database
+APP_SETTING_LOADED = true
 begin
   AppSetting.first
 rescue => ex
-  class AppSetting < ActiveRecord::Base
-    def self.get(str)
-      nil
-    end
-  end
+  APP_SETTING_LOADED = false
+  puts "Note: AppSetting model is yet not loaded"
+  puts "If unhandled, it can raise Exception: #{ex.message}"
+  # class AppSetting < ActiveRecord::Base
+  #   puts "Note: Creating a dummy AppSetting.get method"
+  #   def self.get(str)
+  #     nil
+  #   end
+  # end
 end
 
+if APP_SETTING_LOADED
+  puts "APP_SETTING_LOADED = #{APP_SETTING_LOADED}"
+  #### DEFINING CONSTANTS ###########################
 
-#### DEFINING CONSTANTS ###########################
+  # SITE NAME
+  SITE_NAME = AppSetting.get('site_name')
+  SITE_URL = AppSetting.get('site_url')
+  EMAIL_CUSTOMER_CARE = AppSetting.get('email_customer_care')
 
-# SITE NAME
-SITE_NAME = AppSetting.get('site_name')
-SITE_URL = AppSetting.get('site_url')
-EMAIL_CUSTOMER_CARE = AppSetting.get('email_customer_care')
+  # TITLES
+  TITLE_SITE_HOME = AppSetting.get('title_site_home')
+  TITLE_REGISTER = AppSetting.get('title_register')
+  TITLE_LOGIN = AppSetting.get('title_login')
+  TITLE_LOGOUT = AppSetting.get('title_logout')
+  TITLE_ABOUT_US = AppSetting.get('title_about_us')
+  TITLE_CONTACT_US = AppSetting.get('title_contact_us')
+  TITLE_HELP = AppSetting.get('title_help')
+  TITLE_FAQS = AppSetting.get('title_faqs')
 
-# TITLES
-TITLE_SITE_HOME = AppSetting.get('title_site_home')
-TITLE_REGISTER = AppSetting.get('title_register')
-TITLE_LOGIN = AppSetting.get('title_login')
-TITLE_LOGOUT = AppSetting.get('title_logout')
-TITLE_ABOUT_US = AppSetting.get('title_about_us')
-TITLE_CONTACT_US = AppSetting.get('title_contact_us')
-TITLE_HELP = AppSetting.get('title_help')
-TITLE_FAQS = AppSetting.get('title_faqs')
+  # ACTIVE ADMIN
+  # url: /aadmin
+  # Note: default aadmin id and password is set in *_devise_create_admin_users.rb migration file
+  # AADMIN_ID = AppSetting.get('aadmin_id')
+  # AADMIN_PWD = AppSetting.get('aadmin_pwd')
 
-# ACTIVE ADMIN
-# url: /aadmin
-# Note: default aadmin id and password is set in *_devise_create_admin_users.rb migration file
-# AADMIN_ID = AppSetting.get('aadmin_id')
-# AADMIN_PWD = AppSetting.get('aadmin_pwd')
+  # FACEBOOK SETTINGS
+  # FACEBOOK_APP_ID = AppSetting.get('facebook_app_id')
+  # FACEBOOK_APP_SECRET = AppSetting.get('facebook_app_secret')
 
-# FACEBOOK SETTINGS
-# FACEBOOK_APP_ID = AppSetting.get('facebook_app_id')
-# FACEBOOK_APP_SECRET = AppSetting.get('facebook_app_secret')
+  # DEVISE SETTINGS
+  DEVISE_SECRET_KEY = AppSetting.get('devise_secret_key') || 'Change devise_secret_key'
 
-# DEVISE SETTINGS
-DEVISE_SECRET_KEY = AppSetting.get('devise_secret_key') || 'Change devise_secret_key'
+  # MAILER
+  # MAILER_ADDRESS = AppSetting.get('mailer_address')
+  # MAILER_PORT = AppSetting.get('mailer_port').to_i
+  # MAILER_DOMAIN = AppSetting.get('mailer_domain')
+  # MAILER_USER_NAME = AppSetting.get('mailer_user_name')
+  # MAILER_PASSWORD = AppSetting.get('mailer_password')
+  # MAILER_AUTHENTICATION = AppSetting.get('mailer_authentication')
+  # MAILER_ENABLE_STARTTLS_AUTO = eval("#{AppSetting.get('mailer_enable_starttls_auto')}")
+  # MAILER_DEFAULT_URL_HOST = AppSetting.get('mailer_default_url_host')
+  # MAILER_RAISE_DELIVERY_ERRORS = eval("#{AppSetting.get('mailer_raise_delivery_errors')}")
+  # MAILER_DELIVERY_METHOD = "#{AppSetting.get('mailer_delivery_method')}".to_sym
+  # MAILER_CREDENTIALS = (AppSetting.get('mailer_credentials') ? JSON.parse(AppSetting.get('mailer_credentials')) : nil)
 
-# MAILER
-# MAILER_ADDRESS = AppSetting.get('mailer_address')
-# MAILER_PORT = AppSetting.get('mailer_port').to_i
-# MAILER_DOMAIN = AppSetting.get('mailer_domain')
-# MAILER_USER_NAME = AppSetting.get('mailer_user_name')
-# MAILER_PASSWORD = AppSetting.get('mailer_password')
-# MAILER_AUTHENTICATION = AppSetting.get('mailer_authentication')
-# MAILER_ENABLE_STARTTLS_AUTO = eval("#{AppSetting.get('mailer_enable_starttls_auto')}")
-# MAILER_DEFAULT_URL_HOST = AppSetting.get('mailer_default_url_host')
-# MAILER_RAISE_DELIVERY_ERRORS = eval("#{AppSetting.get('mailer_raise_delivery_errors')}")
-# MAILER_DELIVERY_METHOD = "#{AppSetting.get('mailer_delivery_method')}".to_sym
-# MAILER_CREDENTIALS = (AppSetting.get('mailer_credentials') ? JSON.parse(AppSetting.get('mailer_credentials')) : nil)
-
-# some numeric constants
-# SESSION_TIMEOUT_DURATION = AppSetting.get('session_timeout_duration').to_i # minutes
-
-
+  # some numeric constants
+  # SESSION_TIMEOUT_DURATION = AppSetting.get('session_timeout_duration').to_i # minutes
+else
+  puts "Skipping setting constants defined in Initializer file #{$0}"
+  puts "APP_SETTING_LOADED = #{APP_SETTING_LOADED}"
+end
 
